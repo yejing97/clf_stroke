55fb602589daa451a244b97739d0cdc06448e540
E19B516G <yejing.xie@etu.univ-nantes.fr>
2023-09-28 16:50:13 +0200

    newwwwdd

--- train_edge.py
+++ train_edge.py
@@ -23,7 +23,7 @@ test_loader = torch.utils.data.DataLoader(test_data, batch_size=1, shuffle=True)
 model = mlp.MLP(**model_args).to(torch.device("cuda"))
 
 def train():
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
     criterion = torch.nn.CrossEntropyLoss()
     losses_train = []
     losses_val = []
