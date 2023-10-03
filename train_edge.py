824f9398c1c4046768ff8738b8c478ce6d74f605
E19B516G <yejing.xie@etu.univ-nantes.fr>
2023-10-03 15:42:07 +0200

    save pred

--- train_edge.py
+++ train_edge.py
@@ -55,45 +55,45 @@ def train():
         nb_batch = 0
         nb_val = 0
         acc_sum = 0
-        # model.train()
-        # for fg_emb, gt, id in tqdm(train_loader):
-        #     if fg_emb.size(1) != 0:
-        #         # print(gt.reshape(-1).shape)
-        #         try:
-        #             optimizer.zero_grad()
-        #             output = model(fg_emb.to(torch.device("cuda")))
-        #             # print(output.shape)
-        #             # print(gt.reshape(-1).shape)
-        #             loss = criterion(output, gt.reshape(-1).to(torch.long).to(torch.device("cuda")))
-        #             loss_sum += loss.item()
-        #             nb_batch += 1
-        #             # for name, parms in model.named_parameters():	
-        #             #     print('-->name:', name)
-        #             #     print('-->para:', parms)
-        #             #     print('-->grad_requirs:',parms.requires_grad)
-        #             #     print('-->grad_value:',parms.grad)
-        #             #     print("===")
+        model.train()
+        for fg_emb, gt, id in tqdm(train_loader):
+            if fg_emb.size(1) != 0:
+                # print(gt.reshape(-1).shape)
+                try:
+                    optimizer.zero_grad()
+                    output = model(fg_emb.to(torch.device("cuda")))
+                    # print(output.shape)
+                    # print(gt.reshape(-1).shape)
+                    loss = criterion(output, gt.reshape(-1).to(torch.long).to(torch.device("cuda")))
+                    loss_sum += loss.item()
+                    nb_batch += 1
+                    # for name, parms in model.named_parameters():	
+                    #     print('-->name:', name)
+                    #     print('-->para:', parms)
+                    #     print('-->grad_requirs:',parms.requires_grad)
+                    #     print('-->grad_value:',parms.grad)
+                    #     print("===")
 
 
-        #             loss.backward()
-        #             optimizer.step()
-        #             scheduler.step()
+                    loss.backward()
+                    optimizer.step()
+                    scheduler.step()
 
-        #             # print("=============update===========")
-        #             # for name, parms in model.named_parameters():	
-        #             #     print('-->name:', name)
-        #             #     print('-->para:', parms)
-        #             #     print('-->grad_requirs:',parms.requires_grad)
-        #             #     print('-->grad_value:',parms.grad)
-        #             #     print("===")
-        #             # print(optimizer)
-        #             # input("=====end=====")
+                    # print("=============update===========")
+                    # for name, parms in model.named_parameters():	
+                    #     print('-->name:', name)
+                    #     print('-->para:', parms)
+                    #     print('-->grad_requirs:',parms.requires_grad)
+                    #     print('-->grad_value:',parms.grad)
+                    #     print("===")
+                    # print(optimizer)
+                    # input("=====end=====")
 
-        #         except:
-        #             print(id[0] + ' error!')
-        #             print(fg_emb.shape)
-        # losses_train.append(loss_sum / nb_batch)
-        # print('epoch: {}, loss: {}'.format(epoch, loss_sum / nb_batch))
+                except:
+                    print(id[0] + ' error!')
+                    print(fg_emb.shape)
+        losses_train.append(loss_sum / nb_batch)
+        print('epoch: {}, loss: {}'.format(epoch, loss_sum / nb_batch))
         model.eval()
         for fg_emb, gt, id in tqdm(val_loader):
             if fg_emb.size(1) != 0:
