d48b6973b9d76f07851a010339eea30c55efabaf
E19B516G <yejing.xie@etu.univ-nantes.fr>
2023-10-04 15:08:31 +0200

    dataloader throw expection

--- train_edge.py
+++ train_edge.py
@@ -61,12 +61,13 @@ def train():
         for fg_emb, gt, id in tqdm(train_loader):
             if fg_emb.size(1) != 0:
                 # try:
-                    optimizer.zero_grad()
+                    # optimizer.zero_grad()
                     output = model(fg_emb.to(torch.device("cuda")))
                     loss = criterion(output, gt.reshape(-1).to(torch.long).to(torch.device("cuda")))
                     # print(loss.item())
                     loss_sum += loss.item()
                     nb_batch += 1
+                    optimizer.zero_grad()
                     loss.backward()
                     optimizer.step()
                     scheduler.step()
