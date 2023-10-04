2aab177ac7cc4317178b15dac05f2c851bcb711f
E19B516G <yejing.xie@etu.univ-nantes.fr>
2023-10-04 18:24:24 +0200

    dataloader throw expection

--- train_edge.py
+++ train_edge.py
@@ -42,65 +42,62 @@ if args.model_name == 'mlp':
 else:
     model = svm.SVC(kernel='linear', C=1).to(torch.device("cuda"))
 
-def train():
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size = 100, gamma = 0.1)
-    criterion = torch.nn.CrossEntropyLoss()
-    losses_train = []
-    losses_val = []
-    accs = []
-    for epoch in range(100):
-        # loop = tqdm(, total=100, desc='train')
+optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size = 100, gamma = 0.1)
+criterion = torch.nn.CrossEntropyLoss()
+losses_train = []
+losses_val = []
+accs = []
+for epoch in range(100):
+    # loop = tqdm(, total=100, desc='train')
 
-        loss_sum = 0
-        loss_val = 0
-        nb_batch = 0
-        nb_val = 0
-        acc_sum = 0
-        model.train()
-        for fg_emb, gt, id in tqdm(train_loader):
-            if fg_emb.size(1) != 0:
-                # try:
-                    # optimizer.zero_grad()
-                    output = model(fg_emb.to(torch.device("cuda")))
-                    loss = criterion(output, gt.reshape(-1).to(torch.long).to(torch.device("cuda")))
-                    # print(loss.item())
-                    loss_sum += loss.item()
-                    nb_batch += 1
-                    # optimizer.zero_grad()
-                    # loss.backward()
-                    # optimizer.step()
-                    # scheduler.step()
-                # except:
-                #     print(id[0] + ' error!')
-                #     print(fg_emb.shape)
-        losses_train.append(loss_sum / nb_batch)
-        print('epoch: {}, loss: {}'.format(epoch, loss_sum / nb_batch))
-        model.eval()
-        for fg_emb, gt, id in tqdm(val_loader):
-            if fg_emb.size(1) != 0:
-                try:
-                    output = model(fg_emb.to(torch.device("cuda")))
-                    _, pred = output.max(dim=1)
-                    acc = accuracy_score(gt.reshape(-1), pred.reshape(-1).cpu())
-                    loss = criterion(output, gt.reshape(-1).to(torch.long).to(torch.device("cuda")))
-                    loss_val += loss.item()
-                    acc_sum += acc
-                    nb_val += 1
+    loss_sum = 0
+    loss_val = 0
+    nb_batch = 0
+    nb_val = 0
+    acc_sum = 0
+    model.train()
+    for fg_emb, gt, id in tqdm(train_loader):
+        if fg_emb.size(1) != 0:
+            # try:
+                # optimizer.zero_grad()
+                output = model(fg_emb.to(torch.device("cuda")))
+                loss = criterion(output, gt.reshape(-1).to(torch.long).to(torch.device("cuda")))
+                # print(loss.item())
+                loss_sum += loss.item()
+                nb_batch += 1
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                # scheduler.step()
+            # except:
+            #     print(id[0] + ' error!')
+            #     print(fg_emb.shape)
+    losses_train.append(loss_sum / nb_batch)
+    print('epoch: {}, loss: {}'.format(epoch, loss_sum / nb_batch))
+    model.eval()
+    for fg_emb, gt, id in tqdm(val_loader):
+        if fg_emb.size(1) != 0:
+            try:
+                output = model(fg_emb.to(torch.device("cuda")))
+                _, pred = output.max(dim=1)
+                acc = accuracy_score(gt.reshape(-1), pred.reshape(-1).cpu())
+                loss = criterion(output, gt.reshape(-1).to(torch.long).to(torch.device("cuda")))
+                loss_val += loss.item()
+                acc_sum += acc
+                nb_val += 1
 
-                    if not os.path.exists(args.result_path):
-                        os.makedirs(args.result_path)
-                    torch.save(pred, os.path.join(args.result_path, id[0].split('.')[0] + '.pt'))
-                    # print(os.path.join(args.result_path, id[0].split('.')[0] + '.pt'))
-                except:
-                    print(id)
-                    print(fg_emb.shape)
-        accs.append(acc_sum / nb_val)
-        losses_val.append(loss_val / nb_val)
-        print('epoch: {}, acc: {}, loss: {}'.format(epoch, acc_sum / nb_val, loss_val / nb_val))
+                if not os.path.exists(args.result_path):
+                    os.makedirs(args.result_path)
+                torch.save(pred, os.path.join(args.result_path, id[0].split('.')[0] + '.pt'))
+                # print(os.path.join(args.result_path, id[0].split('.')[0] + '.pt'))
+            except:
+                print(id)
+                print(fg_emb.shape)
+    accs.append(acc_sum / nb_val)
+    losses_val.append(loss_val / nb_val)
+    print('epoch: {}, acc: {}, loss: {}'.format(epoch, acc_sum / nb_val, loss_val / nb_val))
 
 
             # print('epoch: {}, batch: {}, loss: {}'.format(epoch, i, loss.item()))
         # torch.save(model.state_dict(), '/home/e19b516g/yejing/data/EXP/model/mlp.pth')
-
-train()
