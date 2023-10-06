46e7641c0bce544f2c334bb3553da06fe014dc70
E19B516G <yejing.xie@etu.univ-nantes.fr>
2023-10-06 14:57:21 +0200

    batch_train

--- batch_train.sh
+++ batch_train.sh
@@ -2,15 +2,16 @@
 
 # conda activate stroke_clf
 
-python3 train_edge.py --feature_type="FG_EMB" --filter_type="los" --lr=1e-2 --epoches=100
-python3 train_edge.py --feature_type="FR_EMB" --filter_type="los" --lr=1e-2 --epoches=100
+
 python3 train_edge.py --feature_type="FG_EMB" --filter_type="rel" --lr=1e-2 --epoches=100
 python3 train_edge.py --feature_type="FR_EMB" --filter_type="rel" --lr=1e-2 --epoches=100
-python3 train_edge.py --feature_type="FG_EMB" --filter_type="los" --lr=1e-4 --epoches=100
-python3 train_edge.py --feature_type="FR_EMB" --filter_type="los" --lr=1e-4 --epoches=100
 python3 train_edge.py --feature_type="FG_EMB" --filter_type="rel" --lr=1e-4 --epoches=100
 python3 train_edge.py --feature_type="FR_EMB" --filter_type="rel" --lr=1e-4 --epoches=100
-python3 train_edge.py --feature_type="FG_EMB" --filter_type="los" --lr=1e-6 --epoches=100
-python3 train_edge.py --feature_type="FR_EMB" --filter_type="los" --lr=1e-6 --epoches=100
 python3 train_edge.py --feature_type="FG_EMB" --filter_type="rel" --lr=1e-6 --epoches=100
 python3 train_edge.py --feature_type="FR_EMB" --filter_type="rel" --lr=1e-6 --epoches=100
+python3 train_edge.py --feature_type="FG_EMB" --filter_type="los" --lr=1e-2 --epoches=100
+python3 train_edge.py --feature_type="FR_EMB" --filter_type="los" --lr=1e-2 --epoches=100
+python3 train_edge.py --feature_type="FG_EMB" --filter_type="los" --lr=1e-4 --epoches=100
+python3 train_edge.py --feature_type="FR_EMB" --filter_type="los" --lr=1e-4 --epoches=100
+python3 train_edge.py --feature_type="FG_EMB" --filter_type="los" --lr=1e-6 --epoches=100
+python3 train_edge.py --feature_type="FR_EMB" --filter_type="los" --lr=1e-6 --epoches=100
